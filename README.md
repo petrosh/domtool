4a41f82ef254d07a87f4d82b0ea65db6d18cc269
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-01 22:55:37 +0200

    Deploy

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # domtool
 dt-domtool
+
+**To do**
+
+- Object key: string (querySelector)
+- Object value:
+	- string (insertAdjacentHTML, afterbegin)
+	- object
+		- object key: attribute
+		- object value: value
+
+```js
+// Set attributes
+ele.dt({'a':{'href':'http://'}});
+// Append and fill
+ele.dt(['h1',{'h1':'titolo'}]);
+```
---
+++ script.js
@@ -0,0 +1,30 @@
+Element.prototype.dt = function (input) {
+	var parent = this;
+  if (input.constructor === Array) {
+    for (var i = 0; i < input.length; i++) {
+			if (input[i].constructor === Array) this.lastElementChild.dt(input[i]);
+			else if (input[i].constructor === String) this.appendChild(document.createElement(input[i]));
+			else if (input[i].constructor.toString().slice(9,13) === 'HTML') this.appendChild(input[i]);
+    }
+  } else if (input.constructor === String) this.appendChild(document.createElement(input));
+	else if (input.constructor.toString().slice(9,13) === 'HTML') this.appendChild(input);
+	else if (input.constructor === Object) {
+		for (var key in input) {
+		  if (input.hasOwnProperty(key)) {
+				this.querySelector(key).insertAdjacentHTML('afterbegin',input[key]);
+		  }
+		}
+	}
+  return this;
+};
+
+// Create article and fill
+var article = document.createElement('article').dt(['header',['h1',['span'],'p'], 'div',['h3','ul'], 'footer']);
+
+// Append and populate
+document.body.dt(article.dt({'h1':'Botta','h1 span':'daje'}));
+
+// Continue!
+article.querySelector('ul').dt(['li',['span']]);
+article.querySelector('footer').dt(['div',['span'],'p']).dt({'span':'ok'});
+article.dt({'h3':'ora','li span':'QUI!'});
