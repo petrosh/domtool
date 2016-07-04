c3fe52b689bdf809c528a54dcdb1618c42fd3d4d
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-04 21:40:02 +0200

    Script end of document, window.jQuery

--- windowjquery.js
+++ windowjquery.js
@@ -1,8 +1,8 @@
 if (!window.jQuery) {
 	window.location.href="fallback.html";
+} else {
+	$( document ).ready(function() {
+	  $( "p" ).text( "The DOM is now loaded and can be manipulated." );
+		$( "body" ).append($( "p" ).text( "Loaded" ));
+	});
 }
-
-$( document ).ready(function() {
-  $( "p" ).text( "The DOM is now loaded and can be manipulated." );
-	$( "body" ).append($( "p" ).text( "Loaded" ));
-});
