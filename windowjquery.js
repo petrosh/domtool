26ce1c73187b7598782f7583627e415ec05297b2
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-04 23:25:05 +0200

    try ajax

--- windowjquery.js
+++ windowjquery.js
@@ -4,5 +4,8 @@ if (!window.jQuery) {
 	$( document ).ready(function() {
 	  $( "p" ).text( "The DOM is now loaded and can be manipulated." );
 		$( "body" ).append($( "p" ).text( "Loaded" ));
+		$.getJSON('https://api.github.com/repos/petrosh/domtool/git/refs/heads/gh-pages', function(data) {
+			$('body').append($('h2').text('ennamo'));
+		});
 	});
 }
