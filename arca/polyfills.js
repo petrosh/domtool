4511283f35e5429632851ae11de69c44f6a4f824
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-10 17:10:06 +0200

    Introduce polyfills.js

---
+++ arca/polyfills.js
@@ -0,0 +1,31 @@
+if(!("lastElementChild" in document.documentElement)){
+  stack.push('no-lastElementChild');
+  Object.defineProperty(Element.prototype, "lastElementChild", {
+    get: function(){
+      for(var nodes = this.children, n, i = nodes.length - 1; i >= 0; --i)
+        if(n = nodes[i], 1 === n.nodeType) return n;
+      return null;
+    }
+  });
+}
+
+Element.prototype.dt = function (input) {
+	stack.push('constructor: ' + input.constructor.toString());
+	var parent = this;
+  if (input.constructor === Array) {
+    for (var i = 0; i < input.length; i++) {
+			if (input[i].constructor === Array) this.lastElementChild.dt(input[i]);
+			else if (input[i].constructor === String) this.appendChild(document.createElement(input[i]));
+			else if (input[i].constructor === HTMLElement) this.appendChild(input[i]);
+    }
+  } else if (input.constructor === String) this.appendChild(document.createElement(input));
+	else if (input.constructor === HTMLElement) this.appendChild(input);
+	else if (input.constructor === Object) {
+		for (var key in input) {
+		  if (input.hasOwnProperty(key)) {
+				this.querySelector(key).innerHTML += input[key];
+		  }
+		}
+	}
+  return this;
+};
