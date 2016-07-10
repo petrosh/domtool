e7e068b520ef8e7f3cc8da915083c5155797b6b3
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-10 21:09:29 +0200

    Switch to cosa for Screenshot problem

--- cosa/loader.js
+++ cosa/loader.js
@@ -6,7 +6,7 @@ function foo(response){
   var pp = document.createElement('p');
   pp.innerHTML = response.data.object.sha;
   document.body.appendChild(pp);
-	appendScript('https://cdn.rawgit.com/petrosh/domtool/' + response.data.object.sha + '/circa/proceed.js');
+	appendScript('https://cdn.rawgit.com/petrosh/domtool/' + response.data.object.sha + '/cosa/proceed.js');
 }
 
 appendScript('https://api.github.com/repos/petrosh/domtool/git/refs/heads/gh-pages?callback=foo&rnd=' + new Date().getTime());
