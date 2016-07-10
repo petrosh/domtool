7ea992866e7070d7ba7c03c745849ea9ba3e0acd
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-10 18:46:12 +0200

    Remove helper acc

--- arca/helper.js
+++ arca/helper.js
@@ -4,24 +4,6 @@ function appendScript(t) {
 	document.head.appendChild(script);
 }
 
-Element.prototype.acc = function (ele, inner, attributes) {
-	var out = false;
-	if (ele.constructor === String) {
-		var element = document.createElement(ele);
-		if (inner !== '') {
-			element.innerHTML = inner;
-		}
-		if (attributes.constructor === Object) {
-			for (var key in attributes) {
-				if (attributes.hasOwnProperty(key)) {
-					element.setAttribute(key, attributes.key);
-				}
-			}
-		}
-		out = this.appendChild(element);
-	};
-	return out;
-};
 
 if (window.addEventListener)
 	window.addEventListener("load", appendScript, false);
