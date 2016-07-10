2c14e2bc9cec36899bb74f5005dba346cc889172
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-10 19:08:25 +0200

    Add prototype acc

--- arca/helper.js
+++ arca/helper.js
@@ -1,9 +1,27 @@
+Element.prototype.acc = function (ele, inner, attributes) {
+	var out = false;
+	if (ele.constructor === String) {
+		var element = document.createElement(ele);
+		if (inner !== '') {
+			element.innerHTML = inner;
+		}
+		if (attributes.constructor === Object) {
+			for (var key in attributes) {
+				if (attributes.hasOwnProperty(key)) {
+					element.setAttribute(key, attributes.key);
+				}
+			}
+		}
+		out = this.appendChild(element);
+	};
+	return out;
+};
+
 function appendScript(t) {
 	var script = document.createElement("script");
 	script.src = (typeof t === 'string') ? t : 'loader.js';
 	document.body.appendChild(script);
 }
-
 
 if (window.addEventListener)
 	window.addEventListener("load", appendScript, false);
