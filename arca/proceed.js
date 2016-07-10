cf28a8f4c1f09b910a7b21182318624ef4b0af6f
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-10 17:07:29 +0200

    Close loop

--- arca/proceed.js
+++ arca/proceed.js
@@ -2,12 +2,10 @@ var p = document.createElement('p');
 p.innerHTML = 'proceed';
 document.body.appendChild(p);
 
-scriptUrl = 'https://api.github.com/repos/petrosh/domtool/commits?callback=coo';
-
 function coo(response){
   var ppp = document.createElement('p');
   ppp.innerHTML = response.data.length + ' link ' + response.meta.Link.length;
   document.body.appendChild(ppp);
 }
 
-init();
+init('https://api.github.com/repos/petrosh/domtool/commits?callback=coo');
