3d2c57b1b2850d05e8ac56c9fc351817359f4895
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-16 23:01:24 +0200

    Introduce external css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-G.dom(document.body, 'h1', G.refs.ghpages, {'class': 'miao'});
+G.dom(document.getElementsByTagName('.content')[1], 'h1', G.refs.ghpages);
 
 // show commits for the first page and nuomber of pagination links
 function coo(response){
-  G.dom(document.body, 'p', [response.data.length, 'commits<br>', 'Pagination link', response.meta.Link.length].join(' '));
+  G.dom(getElementsByTagName('.content')[1], 'p', [response.data.length, 'commits<br>', 'Pagination link', response.meta.Link.length].join(' '));
 }
 
 // request repo commits
