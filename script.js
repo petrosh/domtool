b699352781e82cf70c507b1e96ecd1e8086440b6
Alex Petrosh <petrosh@users.noreply.github.com>
2016-07-17 11:55:16 +0200

    Introduce commit list and repository link

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-G.dom(document.querySelector('section'), 'h1', G.refs.ghpages);
+var section = document.querySelector('section');
+var footer = document.querySelector('footer');
+G.dom(footer, 'a', 'repository', {href: G.repoUrl});
+G.dom(section, 'h1', ['gh-pages refs head', G.refs.ghpages].join(': '));
+G.dom(section, 'ul');
 
 // show commits for the first page and nuomber of pagination links
 function coo(response){
-  G.dom(document.querySelector('section'), 'p', [response.data.length, 'commits<br>', 'Pagination link', response.meta.Link.length].join(' '));
+  G.repoCommits = response.data;
+  G.dom(section, 'h2', [response.data.length, 'commits<br>', 'Pagination link', response.meta.Link.length].join(' '));
+  for (var i = 0; i < G.repoCommits.length; i++) {
+    G.dom(section.querySelector('ul'), 'li', G.repoCommits[i].commit.message);
+  }
 }
 
 // request repo commits
